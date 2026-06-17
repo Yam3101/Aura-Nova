0458f71d686d79204fc6814ec56da48b3d450d02
yam <angelmay_810@hotmail.com>
2026-06-17 12:21:09 -0500

    multimedia added on galery

--- src/data/media.js
+++ src/data/media.js
@@ -1,6 +1,23 @@
 export const media = {
 	exterior: [
+		{
+			src: "/media/exterior/casa-exterior-foto.jpg",
+			alt: "Vista desde el exterior (Imagen real del lugar, no render)",
+		},
+		{
+			src: "/media/exterior/render-zona-general.jpg",
+			alt: "Vista superior de la zona (Imagen real del lugar, no render)",
+		},
+		{
+			src: "/media/exterior/render-zona-general-2.jpg",
+			alt: "Vista superior de la zona (Imagen real del lugar, no render)",
+		},
+		{
+			src: "/media/exterior/casa-exterior-foto-2.jpg",
+			alt: "Vista desde el exterior (Imagen real del lugar, no render)",
+		},
 		{ src: "/media/exterior/render-zona-1.jpg", alt: "Cancha deportiva" },
+
 		{
 			src: "/media/exterior/render-zona-2.jpg",
 			alt: "Cancha deportiva (vista superior)",
@@ -28,6 +45,38 @@ export const media = {
 		{ src: "/media/interior/bano-1.jpg", alt: "Baño" },
 	],
 	amenidades: [
+		{
+			src: "/media/amenidades/alberca-vista-superior.jpg",
+			alt: "Zona de alberca (Imagen real del lugar, no render)",
+		},
+		{
+			src: "/media/amenidades/alberca-3.jpg",
+			alt: "Alberca (Imagen real del lugar, no render)",
+		},
+		{
+			src: "/media/amenidades/alberca-5.jpg",
+			alt: "Alberca (Imagen real del lugar, no render)",
+		},
+		{
+			src: "/media/amenidades/alberca-4.jpg",
+			alt: "Alberca (Imagen real del lugar, no render)",
+		},
+		{
+			src: "/media/amenidades/palapa.jpg",
+			alt: "Palapa (Imagen real del lugar, no render)",
+		},
+		{
+			src: "/media/amenidades/cancha-superior.jpg",
+			alt: "Cancha, vista superior (Imagen real del lugar, no render)",
+		},
+		{
+			src: "/media/amenidades/area-juegos-1.jpg",
+			alt: "Área de juegos (Imagen real del lugar, no render)",
+		},
+		{
+			src: "/media/amenidades/area-juegos-2.jpg",
+			alt: "Área de juegos (Imagen real del lugar, no render)",
+		},
 		{ src: "/media/amenidades/alberca-1.jpg", alt: "Alberca" },
 		{ src: "/media/amenidades/alberca-2.jpg", alt: "Alberca" },
 		{ src: "/media/amenidades/cancha-alberca-1.jpg", alt: "Cancha y alberca" },
