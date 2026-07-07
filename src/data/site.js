f747a4f01c9a88d89e597fc9d844d0262e34a2b4
yam <angelmay_810@hotmail.com>
2026-07-07 09:00:51 -0500

    official prices updated

--- src/data/site.js
+++ src/data/site.js
@@ -27,8 +27,8 @@ export const site = {
 };
 
 export const prices = [
-	{ label: "3er nivel", valueMXN: 1041000 },
-	{ label: "2do nivel", valueMXN: 1091000 },
-	{ label: "1er nivel", valueMXN: 1141000 },
-	{ label: "Planta baja", valueMXN: 1191000 },
+	{ label: "3er nivel", valueMXN: 1061000 },
+	{ label: "2do nivel", valueMXN: 1111000 },
+	{ label: "1er nivel", valueMXN: 1161000 },
+	{ label: "Planta baja", valueMXN: 1211000 },
 ];
